54b1a8299a0a046b0c821e91b02f4be5020d5b47
Suyeol Jeon <devxoul@gmail.com>
2017-05-01 19:34:19 +0900

    commit

--- Package.swift
+++ Package.swift
@@ -7,13 +7,17 @@ var targets = [
   Target(name: "TestLibA")
 ]
 
-// if let raw = getenv("TEST"), String(validatingUTF8: raw) != nil {
-//   targets.append(Target(name: "TestLibB", dependencies: []))
-// }
+if let raw = getenv("TEST"), String(validatingUTF8: raw) != nil {
+  let target = Target(
+    name: "TestLibB",
+    dependencies: [
+      .Target(name: "TestLibA"),
+    ]
+  )
+  targets.append(target)
+}
 
 let package = Package(
   name: "TestLib",
-  targets: [
-    Target(name: "TestLibA", dependencies: [])
-  ]
+  targets: targets
 )
