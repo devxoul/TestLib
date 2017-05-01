b333b05e9b02d123c627411d693038d735d38afa
Suyeol Jeon <devxoul@gmail.com>
2017-05-01 19:17:38 +0900

    1

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ var targets = [
   Target(name: "TestLibA")
 ]
 
-if let raw = getenv("TEST"), String(validatingUTF8: raw) != nil {
-  targets.append(Target(name: "TestLibB"))
-}
+// if let raw = getenv("TEST"), String(validatingUTF8: raw) != nil {
+//   targets.append(Target(name: "TestLibB"))
+// }
 
 let package = Package(
   name: "TestLib",
